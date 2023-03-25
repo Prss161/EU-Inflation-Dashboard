62fe787eee5fcb3a5414a76d71db498462db5007
Dawid Pruski(Prss161) <dpruski0@gmail.com>
2023-03-25 23:10:08 +0100

    adding description to code

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ df = pd.read_excel(
     nrows = 1000,
 )
 
+#Transposing data table
 df = df.T
 df.columns = df.iloc[0]
 df = df.iloc[1:]
+
+#Streaming transposed data
 st.dataframe(df)
 
-
+#Coding linechart
 fig = px.line(df, x=df.index, y=df.columns, title='Inflation by Country')
 fig.update_xaxes(title='Time')
 fig.update_yaxes(title='Inflation')
